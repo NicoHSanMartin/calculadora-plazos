d806e16e63bd33aedacf7acfe802a55800bf31e5
NicoHSanMartin <niico.net@gmail.com>
2026-05-21 10:42:33 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = "plazos-oj-v1";
+const FILES = [
+  "./calculadora_judicial_corrientes.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.addAll(FILES); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).catch(function() { return cached; });
+    })
+  );
+});
